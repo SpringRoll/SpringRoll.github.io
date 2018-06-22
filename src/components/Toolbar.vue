b22d62ed0bea1211111eb337328f830b4986bdde
Andrew Smith <andrew.smith@redspace.com>
2018-06-22 14:14:21 -0300

    removed nav bar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,6 +1,3 @@
 <template>
-  <v-toolbar app color="primary">
-    <v-toolbar-side-icon @click="$emit('nav')"></v-toolbar-side-icon>
-    <v-toolbar-title>Springroll.io</v-toolbar-title>
-  </v-toolbar>
+  <v-toolbar-side-icon @click="$emit('nav')"></v-toolbar-side-icon>
 </template>
