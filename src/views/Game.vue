79a14f0562c28632b945543b15ca3c01a1eb1154
Andrew Smith <andrew.smith@redspace.com>
2018-08-27 14:36:43 -0300

    added docs dir for github io

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,23 +1,71 @@
 <template>
   <div class="game__container">
-    <iframe class="game" :src="base + 'game/index.html'" frameborder="0" />
+    <iframe id="demo-game" class="game" :src="base + 'game/index.html'" frameborder="0" />
+    <div class="game__events">
+      <h2>Game Events</h2>
+      <div v-for="(event, key) in events" :key=key :class="{'--active': event.on }" class="game__event">{{event.label}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-
 import Iframe from '@/mixins/Iframe';
+import { Bellhop } from 'bellhop-iframe';
+
 export default {
-  mixins: [Iframe]
+  mixins: [Iframe],
+  data() {
+    return {
+      bellhop: new Bellhop('demo-game'),
+      events: {
+        'localizerResolve': { label: 'Localiztion', on: false},
+        'speechSynthStart': { label: 'Speech Synch',  on: false },
+        'pauseScreenActive': { label: 'Pause',  on: false },
+        'soundMute': { label: 'Sound Mute',  on: false },
+        'captionStart': { label: 'Captions Start',  on: false },
+      }
+    };
+  },
+  mounted() {
+    const key = Object.keys(this.events);
+
+
+    for (let i = 0, l = key.length; i < l; i++) {
+      this.bellhop.on(key[i], () => {
+        this.events[key[i]].on = true;
+
+        setTimeout(() => {
+          this.events[key[i]].on = false;
+        }, (250));
+      });
+    }
+
+    this.bellhop.connect(document.getElementById('demo-game'));
+  }
 };
 
 </script>
 
 
 <style lang="scss">
+@import '~@/scss/colors';
+@import '~@/scss/mixins';
 .game {
   height: 52rem;
   width: 65rem;
+
+
+  &__events {
+    color: $accent;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__event {
+    @include key(25rem);
+  }
 
   &__container {
     align-items: center;
